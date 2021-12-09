2a490b39cc5957eab50b7694c19ba2fcb89e8c97
Sylvie-B <unsolo@hotmail.fr>
2021-12-09 15:45:24 +0100

    dfghjkl

--- exercice1.js
+++ exercice1.js
@@ -6,6 +6,7 @@ let countUp = function() {
 
 // How can you make it stop counting?
 let stopCountUp = function() {
+
 };
 let stopButton = document.getElementById("stop-button");
 stopButton.addEventListener("click", stopCountUp);
