2a1c34d5670f466e101cd4f50bfe7880ede78fb3
Sylvie-B <unsolo@hotmail.fr>
2021-12-09 22:48:11 +0100

    chrono + stop

--- exercice1.js
+++ exercice1.js
@@ -1,13 +1,23 @@
 // Make it count up on a timer, calling this function
 let seconds = document.getElementById("seconds");
-let countUp = function() {
-    // ?
-};
-
-// How can you make it stop counting?
-let stopCountUp = function() {
-
-};
 let stopButton = document.getElementById("stop-button");
+
+let i = 0;
+let count;
+
+let countUp = function() {
+    count = setTimeout(function (){
+        seconds.innerHTML = i.toString();
+        i++;
+        countUp();
+    }, 1000);
+};
+
+let stopCountUp = function() {
+    clearInterval(count);
+};
+
+countUp();
+// How can you make it stop counting?
 stopButton.addEventListener("click", stopCountUp);
 
